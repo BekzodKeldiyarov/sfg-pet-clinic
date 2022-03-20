193d243f0322589b777c2e6aa2ff21091a2dd179
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-21 03:13:31 +0500

    Multi-Module Maven Builds . closes #3

---
+++ pet-clinic-web/src/main/java/com/bekzodkeldiyarov/springpetproject/SpringPetProjectApplication.java
@@ -0,0 +1,14 @@
+//package com.bekzodkeldiyarov.springpetproject;
+package com.bekzodkeldiyarov.springpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetProjectApplication.class, args);
+    }
+
+}
