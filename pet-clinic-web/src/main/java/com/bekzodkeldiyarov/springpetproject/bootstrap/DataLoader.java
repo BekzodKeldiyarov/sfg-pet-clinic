b229bad9363f3401ee7be0c84bf704cb5e2c0ff6
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-21 23:17:18 +0500

    Bootstrap data added. closes #14

---
+++ pet-clinic-web/src/main/java/com/bekzodkeldiyarov/springpetproject/bootstrap/DataLoader.java
@@ -0,0 +1,39 @@
+package com.bekzodkeldiyarov.springpetproject.bootstrap;
+
+import com.bekzodkeldiyarov.springpetproject.model.Owner;
+import com.bekzodkeldiyarov.springpetproject.services.OwnerService;
+import com.bekzodkeldiyarov.springpetproject.services.VetService;
+import com.bekzodkeldiyarov.springpetproject.services.map.OwnerServiceMap;
+import com.bekzodkeldiyarov.springpetproject.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner1);
+        ownerService.save(owner2);
+
+        System.out.println("Owners loaded...");
+
+    }
+}
