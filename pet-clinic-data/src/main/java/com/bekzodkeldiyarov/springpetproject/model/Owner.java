47930c7b1c2bcfebd01f9897d252887994511cde
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-24 01:02:18 +0500

    Created Visit Entity, updated owner with pet relationship. Closes #28

--- pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/model/Owner.java
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/model/Owner.java
@@ -1,5 +1,14 @@
 package com.bekzodkeldiyarov.springpetproject.model;
 
-public class Owner extends Person{
+import java.util.Set;
 
+public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Owner() {
+    }
+
+    public Owner(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
