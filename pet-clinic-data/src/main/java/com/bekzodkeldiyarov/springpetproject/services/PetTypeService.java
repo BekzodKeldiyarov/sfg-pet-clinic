2985b042e3198d1192cfa36eee8e0967dd9e66b1
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-24 01:15:50 +0500

    Created PetType Map Service and Implementation. Closes #31

---
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/services/PetTypeService.java
@@ -0,0 +1,8 @@
+package com.bekzodkeldiyarov.springpetproject.services;
+
+import com.bekzodkeldiyarov.springpetproject.model.PetType;
+import com.bekzodkeldiyarov.springpetproject.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
