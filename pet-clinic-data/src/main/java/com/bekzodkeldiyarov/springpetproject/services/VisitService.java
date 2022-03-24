69b307539aee6cdce97bb82c8e66fee31e707894
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-25 02:09:05 +0500

    Added VisitService, VisitServiceMap. Closes #48

---
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.bekzodkeldiyarov.springpetproject.services;
+
+import com.bekzodkeldiyarov.springpetproject.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
