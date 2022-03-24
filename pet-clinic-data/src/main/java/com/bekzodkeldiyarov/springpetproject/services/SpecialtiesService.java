2e7e3ddac2f4431349d2b07c5467dff1d4d1db14
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-24 11:10:57 +0500

    Added Specialty Service and Implementation. Closes #36

---
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.bekzodkeldiyarov.springpetproject.services;
+
+import com.bekzodkeldiyarov.springpetproject.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
