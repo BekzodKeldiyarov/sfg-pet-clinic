6527bcd86b3b3f87535191bcc888bcea3b23df74
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-25 01:38:15 +0500

    Added VetSDJpaService. Closes #43

---
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.bekzodkeldiyarov.springpetproject.services.springdatajpa;
+
+import com.bekzodkeldiyarov.springpetproject.model.Vet;
+import com.bekzodkeldiyarov.springpetproject.repositories.VetRepository;
+import com.bekzodkeldiyarov.springpetproject.services.VetService;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class VetSDJpaService implements VetService {
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        Optional<Vet> vetOptional = vetRepository.findById(id);
+        if (vetOptional.isPresent()) {
+            return vetOptional.get();
+        }
+        return null;
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
