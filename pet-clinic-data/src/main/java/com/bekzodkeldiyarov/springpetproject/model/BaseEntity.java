bae5d306f0f6bf379da6edf0c39e950f0df37da0
bekzodkeldiyarov <bek.bek.bek.bek72@gmail.com>
2022-03-28 17:44:08 +0500

    Added constructors for BaseEntity. Added id argument for Owner and Person constructors.

--- pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/bekzodkeldiyarov/springpetproject/model/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.bekzodkeldiyarov.springpetproject.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +11,8 @@ import java.io.Serializable;
 @Setter
 @Getter
 @MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
